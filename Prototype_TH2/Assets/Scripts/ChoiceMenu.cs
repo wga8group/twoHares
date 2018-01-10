d1bacf4b9a181bba8c18cc151def2bced343fb2c
Gichik <ilia1410@ya.ru>
2018-01-10 17:42:56 +0300

    Добавил менюшку
    Signed-off-by: Gichik <ilia1410@ya.ru>

---
+++ Prototype_TH2/Assets/Scripts/ChoiceMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChoiceMenu : MonoBehaviour {
+
+    //варианты (index - порядковый индекс сцены в build settings; на момент написания кода у SEM это 2)
+    //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + index);
+    //SceneManager.LoadScene("SEM");
+    //SceneManager.LoadScene(index);
+    //SceneManager.LoadScene("Scenes/SEM/SEM");
+
+
+    public void PlaySem()
+    {
+        SceneManager.LoadScene("Scenes/SEM/SEM");
+    }
+
+    public void PlayVov()
+    {
+        SceneManager.LoadScene("Scenes/VOV/VOV");
+    }
+
+    public void PlayAlex()
+    {
+        SceneManager.LoadScene("Scenes/ALEX/ALEX");
+    }
+
+    public void PlayAnt()
+    {
+        SceneManager.LoadScene("Scenes/ANT/ANT");
+    }
+
+    public void PlayKris()
+    {
+        SceneManager.LoadScene("Scenes/KRIS/KRIS");
+    }
+
+    public void PlayAnn()
+    {
+        SceneManager.LoadScene("Scenes/ANN/ANN");
+    }
+
+    public void PlayIlia () {
+        SceneManager.LoadScene("Scenes/ILIA/ILIA");
+    }
+
+    // Update is called once per frame
+    public void QuiteGame () {
+        Debug.Log("QUIET!");
+        Application.Quit();
+	}
+}
