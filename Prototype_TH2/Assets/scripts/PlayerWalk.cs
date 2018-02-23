cb3ea772b603564867e356447855a34867373ce9
Gichik <ilia1410@ya.ru>
2018-02-23 21:02:04 +0300

    персонаж ходит влево-вправо по клику мышки (Scenes/ILIA/Test111.unity)

--- Prototype_TH2/Assets/scripts/PlayerWalk.cs
+++ Prototype_TH2/Assets/scripts/PlayerWalk.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerWalk : MonoBehaviour {
 
     [SerializeField]
-    private float speed = 10.0F;
+    private float speed = 1.0F;
+
+    public float moveTime = 0.1F;
 
     private Animator animator;
     private SpriteRenderer sprite;
+    private Vector3 mousePosition;
+    private bool move;
 
     private PlayerState State
     {
@@ -21,15 +25,37 @@ public class PlayerWalk : MonoBehaviour {
     {
         animator = GetComponent<Animator>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        mousePosition = transform.position;
+        move = false;
     }
 
     private void Update()
     {
+        
         State = PlayerState.Idle;
 
         if (Input.GetButton("Horizontal")) {
             Walk();
-        }    
+        }
+
+
+        /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 500000))
+        {
+
+        }*/
+        if (move == true)
+        {
+            MouseWalk();
+        }
+
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetMousePosition();
+            move = true;
+        }
     }
 
     private void Walk()
@@ -41,6 +67,45 @@ public class PlayerWalk : MonoBehaviour {
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
         sprite.flipX = direction.x < 0.0F;
     }
+
+
+    private void SetMousePosition()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+    }
+
+
+    private void MouseWalk()
+    {
+        State = PlayerState.Walk;
+        //Vector2 mousePos = new Vector2();
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        // mousePos.x = Input.mousePosition.x;
+        //mousePos.y = c.pixelHeight - Input.mousePosition.y;
+        //Debug.Log("Screen pixels: " + c.pixelWidth + ":" + c.pixelHeight);
+        //Debug.Log("Mouse position: " + mousePos);
+        //Debug.Log("World position: " + mousePosition.ToString("F3"));
+        //Debug.Log("Transform position: " + transform.position.ToString("F3"));
+
+        Vector3 direction = new Vector3(1, 0, 0);
+        if (mousePosition.x < transform.position.x)
+        {
+            direction.x = direction.x*(-1);
+        }
+
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+
+        if (transform.position.x >= mousePosition.x - 0.02F && transform.position.x <= mousePosition.x + 0.02F) 
+        {
+            move = false;
+        }
+      
+
+    }
+
+
 }
 
 
